b0051a3829ec2f29d2a4420fcb7b14451a331f3b
Kaybhee <abdulkabirbadru@gmail.com>
2026-09-03 13:18:56 +0100

    Fix: Errors with file structure

--- repository/base.repository.ts
+++
@@ -1,8 +0,0 @@
-import { QueryOptions, UpdateWriteOpResult } from 'mongoose';
-import { Model, FilterQuery, UpdateQuery } from 'mongoose';
-
-export class BaseRepository<T> {
-  constructor(private readonly model: Model<T>) {}
-
-  // Centralized CRUD databse operations for all entities, to avoid code duplication
-}
